5c519d1d45827e60d1a6fab3202c8f7250777bfc
Elizabeth "Lizzie" Siegle <lizzie.siegle@gmail.com>
2016-06-07 09:53:36 -0700

    Initial Commit

---
+++ piHeartRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  piHeartRate
+//
+//  Created by Elisabeth Siegle on 6/7/16.
+//  Copyright © 2016 Lizzie Siegle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
